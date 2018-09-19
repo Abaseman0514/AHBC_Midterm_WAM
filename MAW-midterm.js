606490c0833e3edc67a040177742482f55e1493b
Ariana Baseman <ArianaMB@yahoo.com>
2018-09-19 18:19:44 -0400

    AMB ready to share

---
+++ MAW-midterm.js
@@ -0,0 +1,39 @@
+$(".blues").on("click", () => {
+	$("#blues").toggle();
+})
+
+$(".jazz").on("click", () => {
+	$("#jazz").toggle();
+	})
+
+$(".country").on("click", () => {
+	$("#country").toggle();
+})
+
+$(".motown").on("click", () => {
+	$("#motown").toggle();
+})
+
+$(".folk").on("click", () => {
+	$("#folk").toggle();
+})
+
+$(".fountain").on("click", () => {
+	$("#fountain").toggle();
+})
+
+$(".canned").on("click", () => {
+	$("#canned").toggle();
+})
+
+$(".fries").on("click", () => {
+	$("#fries").toggle();
+})
+
+$(".onion").on("click", () => {
+	$("#onion").toggle();
+})
+
+$(".tots").on("click", () => {
+	$("#tots").toggle();
+})
