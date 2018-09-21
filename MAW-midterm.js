34fe41f08aea3c447a8b5639ccfaea8838d651c8
Ariana Baseman <ArianaMB@yahoo.com>
2018-09-20 21:52:37 -0400

    after Thursday

--- MAW-midterm.js
+++ MAW-midterm.js
@@ -1,51 +1,61 @@
 $(".jazz").on("click", () => {
-	$("#jazz").toggle();
+    $("#jazz").toggle();
 })
 
 $(".blues").on("click", () => {
-	$("#blues").toggle();
+    $("#blues").toggle();
 })
 
 $(".country").on("click", () => {
-	$("#country").toggle();
+    $("#country").toggle();
 })
 
 $(".motown").on("click", () => {
-	$("#motown").toggle();
+    $("#motown").toggle();
 })
 
 $(".folk").on("click", () => {
-	$("#folk").toggle();
+    $("#folk").toggle();
 })
 
 $(".fountain").on("click", () => {
-	$("#fountain").toggle();
+    $("#fountain").toggle();
 })
 
 $(".canned").on("click", () => {
-	$("#canned").toggle();
+    $("#canned").toggle();
 })
 
 $(".fries").on("click", () => {
-	$("#fries").toggle();
+    $("#fries").toggle();
 })
 
 $(".onion").on("click", () => {
-	$("#onion").toggle();
+    $("#onion").toggle();
 })
 
 $(".tots").on("click", () => {
-	$("#tots").toggle();
+    $("#tots").toggle();
 })
 
-$(".plus").on("click", function() {
-	$('.items').append($(this).siblings()[0].innerText);
-    // if(event.target.className === "plus"){
-    //     const li = document.createElement('li'); {
-    //     li.innerText = order <button class="remove">Remove</button> 
+let subtotal = 0;
+$(".plus").on("click", function () {
+   $('.items').append($(this).siblings()[0].innerText);
+   subtotal += parseFloat($(this).siblings()[1].innerText.trim().substring(1)); 
+   $('.preTax').append(subtotal);
+})
+
+
+
+    // if (event.target.className === "plus") {
+    //     const li = document.createElement('li');
+    //     li.innerHTML = `${orderList.value} <button class = "remove">Remove</button>`
     //     ul.appendChild(li);
-    })
-        
+    //     orderList.value = "";
+    // }
+// })
+//        });
+
 //if item shows aka true, add remove button to menu on item
 
 //^^^^HOW TO MAKE THIS A LIST^^^
@@ -57,78 +67,75 @@ $(".plus").on("click", function() {
 
 //  }
 // });
+// $('#itemsAdded').hide();
 
-$('.overall').hide();
+$('#review').hide(); 
 
-$('#checkout').on('click', function(){
-$('.overall').show();
-});
+$('#checkout').on("click", function(){
+    $('#review').toggle();
+})
 
-$('.calculate').on('click', function(){
+$('#cashPay').hide();
+$('#creditPay').hide();
+
+// $('#submit').on('click', function () {
+//     $('#itemsAdded').show();
+// // $('.overall').hide();
+
+// $('#checkout').on('click', function () {
+//     $('.overall').show();
+// });
+
+$('.calculate').on('click', function () {
     let provide = document.getElementById('provided').value;
-    let due = document.getElementById('owed').value; 
-    let change = parseFloat((provide - due)); 
+    let due = document.getElementById('owed').value;
+    let change = parseFloat((provide - due));
     window.alert(`Customer is due back $${change} in change`);
 });
 
-$('.submit').on('click', function(){
-    window.alert('Thank you for your payment'); 
-//    if time add error messages for incorrect inputs
+$('.submit').on('click', function () {
+    window.alert('Thank you for your payment');
+    //    if time add error messages for incorrect inputs
 });
 
-$('.receipt').on('click', function(){
-    window.alert(`${order}`); 
+$('.receipt').on('click', function () {
+    window.alert(`${order}`);
 });
 
-const jazzClub = 9.50;
-const fries = 4.24;
-const water = 2;
-const taxs =.06;
-
-function add(a, b, c){
-   return a + b + c;
-    
-}
-
-function sum() {
-    var n = 0;
-    for (var i=0; i < arguments.length; i++) {
-        n += arguments[i];
-    }
-    return n;
-}//function to add
 
 
-
-let brown = add(jazzClub,fries, water);
-
-$('.removeItem').on('click', function(){
+$('.removeItem').on('click', function () {
     $('li:current').detach();
 
 });
 
-$('#checkout').one('click', function(){
-    $('.subTotal').append("  ", brown);
-    $('.total').append("  ", brown * taxs + brown);
-    $('.tax').append(" ", brown * taxs);
-});//checkout button that adds and multiplies totals
+// $('#checkout').one('click', function () {
+//     $('.subTotal').append("  ", brown);
+//     $('.total').append("  ", brown * taxs + brown);
+//     $('.tax').append(" ", brown * taxs);
+// }); //checkout button that adds and multiplies totals
 
-$('#cash').on('click', function(){
-    $('cash').hide();
+$('#payCash').on('click', function () {
+    $('#cashPay').toggle();
 });
+
+$('#payCredit').on('click', function () {
+    $('#creditPay').toggle();
+});
+
 
 
 
 
 $.get('http://www.flyingmusic.com/archives/2015-2014/know-facts-motown/'), (data) => {
-  
+
     console.log(data.ul);
-  };//error cors
+}; //error cors
 
 
 // $('.removeItem').on('click', function(){
 //     $('li').$(this).slideToggle().hide();
-    
+
 // });
 
 let addItem;
